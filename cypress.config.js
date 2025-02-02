b09cd04c587ac5d65d0558bc9dcd8afdb9811652
Alla Varavina <fistawka27@gmail.com>
2025-02-02 16:48:52 +0100

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    chromeWebSecurity: false,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-    },
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports/mocha',
-      overwrite: false,
-      html: false,
-      json: true,
-      charts: true,
+      return config;
     },
     env: {
       BASE_URL: 'https://www.saucedemo.com',
